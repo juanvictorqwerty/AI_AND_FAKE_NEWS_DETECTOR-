9c27d376cc5d99a2717bc0ed66df79039773c6ae
Juan <juanvictorqwerty@gmail.com>
2026-03-21 00:16:52 +0100

    minor style color improved

--- ai_fake_news_detector/lib/utils/global.colors.dart
+++ ai_fake_news_detector/lib/utils/global.colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class GlobalColors {
-  static const Color mainColor = Color.fromARGB(255, 61, 89, 228);
+  static const Color mainColor = Color.fromARGB(255, 3, 72, 248);
   static const Color textColor = Color.fromARGB(255, 21, 21, 21);
 }
