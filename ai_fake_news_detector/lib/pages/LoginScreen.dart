e02513315ff841acedc283d528239402f862b564
Juan <juanvictorqwerty@gmail.com>
2026-03-21 00:29:30 +0100

    Oh by the way...make sure login and Sign are arrows free

--- ai_fake_news_detector/lib/pages/LoginScreen.dart
+++ ai_fake_news_detector/lib/pages/LoginScreen.dart
@@ -13,6 +13,7 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: GlobalColors.mainColor,
         title: Text(
           "AI & FAKE NEWS DETECTOR",
