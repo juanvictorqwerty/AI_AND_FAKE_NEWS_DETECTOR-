d9482123e6c84256c5554835e2fe4bdcc3c9c35b
Juan <juanvictorqwerty@gmail.com>
2026-03-21 19:24:58 +0100

    start home page

--- ai_fake_news_detector/lib/pages/HomePage.dart
+++ ai_fake_news_detector/lib/pages/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:ai_fake_news_detector/utils/global.colors.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
@@ -5,9 +6,45 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        "Hello World"
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: GlobalColors.mainColor,
+        title: Text(
+          "AI & FAKE NEWS DETECTOR",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            height: 50
+          ),
+        ),
+        actions: <Widget>[
+          IconButton( 
+            icon: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            onPressed: (){
+
+            },
+            )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child:Container(
+            width: double.infinity,
+              padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  mainAxisAlignment:MainAxisAlignment.center ,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Hello world")
+                  ],
+                ),
+            )
+        ),
       ),
     );
   }
