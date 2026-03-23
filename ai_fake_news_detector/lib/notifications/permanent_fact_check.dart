65e418c99cced6f76cd990eda1e223180fbed07b
Juan <juanvictorqwerty@gmail.com>
2026-03-23 17:50:02 +0100

    adding a the permanent notif premice

---
+++ ai_fake_news_detector/lib/notifications/permanent_fact_check.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PermanentFactCheck extends StatefulWidget {
+  const PermanentFactCheck({super.key});
+
+  @override
+  State<PermanentFactCheck> createState() => _PermanentFactCheckState();
+}
+
+class _PermanentFactCheckState extends State<PermanentFactCheck> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
