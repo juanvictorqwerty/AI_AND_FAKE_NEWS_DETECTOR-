dc4dee0551d765c70c99c5c8db79a9344ee0160c
Juan <juanvictorqwerty@gmail.com>
2026-05-21 13:27:20 +0100

    feat: add new components for About Us, Guide, and Objectives pages; integrate Actual Users count; update Navbar and Hero sections; enhance layout with new dependencies

---
+++ website/src/app/AboutUs/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import AboutUs from "@/components/AboutUs";
+
+export default function AboutUSPage() {
+    return (
+        <main>
+            <AboutUs/>
+        </main>
+    );
+}
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,18 +1,27 @@
 import Hero from "@/components/Hero";
 import applogo from "../../public/applogo.png";
+import ActualUsers from "@/components/actualUsers";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="min-h-screen flex flex-col items-center bg-linear-to-b from-zinc-50 to-white dark:from-black dark:to-zinc-900 font-sans">
+      
+      <main className="w-full max-w-5xl px-6 py-16 flex flex-col items-center">
+        
+        {/* Logo */}
         <img
-          className="dark:invert"
+          className="mb-6 dark:invert"
           src={applogo.src}
           alt="App logo"
-          width={100}
-          height={20}
+          width={120}
         />
+
+        {/* Hero */}
         <Hero />
+
+        {/* Users */}
+        <ActualUsers />
+
       </main>
     </div>
   );
